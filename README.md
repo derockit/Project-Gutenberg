b7d1d673f07214a40fb9125d402eb01adeb8d022
Sajjad shirazy <shirazy.sajjad@gmail.com>
2022-04-10 12:03:38 +0430

    feat: initialize

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# project-gutenberg
+# Project Gutenberg Ebooks
+
+A redistribution of project Gutenberg. It includes English, not copyrighted ones.
+
+## Usage
+
+The `index.csv` file indexes existing files & their paths. You can generate the paths by the book ids yourselves too:
+
+```javascript
+import md5 from 'md5';
+
+const hash = md5(bookId);
+const path = `${hash[0]}/${hash[1]}/${hash[2]}/${hash}`;
+```
+
+## Thanks to
+
+1. [Project Gutenberg](https://www.gutenberg.org/)
+2. [Gutendex](https://gutendex.com/)
---
+++ crawler.js
@@ -0,0 +1,141 @@
+// @ts-check
+import {
+  appendFileSync,
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+} from 'fs';
+import md5 from 'md5';
+import fetch from 'node-fetch';
+import json2md from 'json2md';
+import exec from 'await-exec';
+
+const STATUS_FILE_PATH = './status.json';
+const INDEX_FILE_PATH = './index.csv';
+
+export class Crawler {
+  get status() {
+    return JSON.parse(readFileSync(STATUS_FILE_PATH, { encoding: 'utf8' }));
+  }
+
+  set status(value) {
+    writeFileSync(STATUS_FILE_PATH, JSON.stringify(value), {
+      encoding: 'utf8',
+    });
+  }
+
+  async start() {
+    let next = null;
+    do {
+      const page = await this.fetchNextPage();
+      const items = page.results.filter((item) => {
+        return (
+          !item.copyright &&
+          item.languages?.includes('en') &&
+          Object.keys(item.formats).includes('application/epub+zip')
+        );
+      });
+      for (const item of items) {
+        await this.save(item);
+      }
+      this.status = { lastPage: this.status.lastPage + 1 };
+      next = page.next;
+    } while (next);
+  }
+
+  async commitChanges({ id, title }) {
+    title = title.replace('"', `'`);
+    const { stdout } = await exec(
+      `git add --all && git commit -m "[${id}] ${title}" && git push`
+    );
+    console.log(stdout);
+  }
+
+  /**
+   *
+   * @returns {{ next: string; results: any[] }}
+   */
+  fetchNextPage() {
+    // @ts-ignore
+    return fetch(
+      `https://gutendex.com/books/?page=${this.status.lastPage + 1}`
+    ).then((res) => res.json());
+  }
+
+  /**
+   *
+   * @param {string} hash
+   */
+  hashToPath(hash) {
+    return `${hash[0]}/${hash[1]}/${hash[2]}/${hash}`;
+  }
+
+  async save(item) {
+    const hash = md5(item.id);
+    const directory = this.hashToPath(hash);
+    if (existsSync(`${directory}/README.md`)) {
+      console.log(`Exists: [${item.id}] ${item.title}`);
+      return;
+    }
+    console.log(`Started: [${item.id}] ${item.title} ...`);
+    mkdirSync(directory, { recursive: true });
+    await this.saveAssets(item, directory);
+    writeFileSync(`${directory}/meta.json`, JSON.stringify(item), {
+      encoding: 'utf8',
+    });
+    writeFileSync(`${directory}/README.md`, this.createReadme(item), {
+      encoding: 'utf8',
+    });
+    appendFileSync(INDEX_FILE_PATH, `\n${item.id},${directory}`);
+    await this.commitChanges(item);
+    console.log(`Added: [${item.id}] ${item.title}`);
+    console.log(Array(20).fill('-').join(''));
+  }
+
+  async saveAssets(item, directory) {
+    const epubFile = await fetch(item.formats['application/epub+zip']).then(
+      (res) => res.buffer()
+    );
+    writeFileSync(`${directory}/ebook.epub`, epubFile);
+
+    const smallCover = await fetch(
+      `https://www.gutenberg.org/cache/epub/${item.id}/pg${item.id}.cover.small.jpg`
+    ).then((res) => res.buffer());
+    writeFileSync(`${directory}/cover.small.jpg`, smallCover);
+
+    const mediumCover = await fetch(
+      `https://www.gutenberg.org/cache/epub/${item.id}/pg${item.id}.cover.medium.jpg`
+    ).then((res) => res.buffer());
+    writeFileSync(`${directory}/cover.medium.jpg`, mediumCover);
+  }
+
+  createReadme(item) {
+    return json2md([
+      { h1: `${item.title} <kbd>${item.id}</kbd>` },
+      { img: { source: './cover.medium.jpg' } },
+      { h2: 'Authors' },
+      {
+        ul: item.authors.map(
+          ({ name, birth_year, death_year }) =>
+            `${name} <small>(${birth_year} - ${death_year})</small>`
+        ),
+      },
+      { h2: 'Subjects' },
+      { ul: item.subjects },
+      { h2: 'Download' },
+      {
+        ul: Object.keys(item.formats).map((key) => item.formats[key]),
+      },
+      { h2: 'Book Shelves' },
+      { ul: item.bookshelves },
+    ]);
+  }
+}
+
+async function main() {
+  const crawler = new Crawler();
+  await crawler.start();
+}
+
+main().catch(console.error);
